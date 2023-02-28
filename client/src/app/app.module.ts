2ff955f0591c74e230750165d10ec5b3de94e514
Alwin Lor <lor00148@morris.umn.edu>
2023-02-28 14:30:58 -0600

    Fix import/name issues
    change import in app.module.ts
    change expected name in app.component.spec.ts

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -30,6 +30,7 @@ import { HomeComponent } from './home/home.component';
 import { UserCardComponent } from './users/user-card.component';
 import { UserListComponent } from './users/user-list.component';
 import { UserProfileComponent } from './users/user-profile.component';
+import { RequestListComponent } from './requests/request-list.component';
 
 const MATERIAL_MODULES: any[] = [
   MatButtonModule,
@@ -58,6 +59,7 @@ const MATERIAL_MODULES: any[] = [
     UserCardComponent,
     UserProfileComponent,
     AddUserComponent,
+    RequestListComponent,
   ],
   imports: [
     BrowserModule,
