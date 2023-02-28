39d6ffc248974536314b8952ed2c668191875ade
Alwin Lor <lor00148@morris.umn.edu>
2023-02-28 14:31:43 -0600

    Add request service file
    Add filter for getting requests

---
+++ client/src/app/requests/request.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Request, ItemType, FoodType } from './request';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RequestService {
+  // The URL for the requests part of the server API
+  readonly requestUrl: string = `${environment.apiUrl}requests`;
+
+  private readonly itemTypeKey = 'itemType';
+  private readonly foodTypeKey = 'foodType';
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  getRequests(filters?: {itemType?: ItemType; foodType?: FoodType}): Observable<Request[]> {
+    let httpParams: HttpParams = new HttpParams();
+    if (filters) {
+      if (filters.itemType) {
+        httpParams = httpParams.set(this.itemTypeKey, filters.itemType);
+      }
+      if (filters.foodType) {
+        httpParams = httpParams.set(this.foodTypeKey, filters.foodType);
+      }
+    }
+
+    return this.httpClient.get<Request[]>(this.requestUrl, {
+      params: httpParams,
+    });
+  }
+}
