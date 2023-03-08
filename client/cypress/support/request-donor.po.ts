b951e2a9aa3c707d72ae44f951d5ee0156e0b3b6
Alwin Lor <lor00148@morris.umn.edu>
2023-03-07 21:23:56 -0600

    Add a support file for e2e
    Added support file for donor view e2e testing

--- client/cypress/support/request-donor.po.ts
+++ client/cypress/support/request-donor.po.ts
@@ -2,7 +2,7 @@ import { ItemType, FoodType } from 'src/app/requests/request';
 
 export class RequestDonorPage {
   private readonly baseUrl = '/requests/donor';
-  private readonly pageTitle = 'donor-view-title';
+  private readonly pageTitle = '.donor-view-title';
   private readonly requestItemTypeDropDown = '[data-test=requestItemTypeSelect]';
   private readonly requestFoodTypeDropDown = '[data-test=requestItemTypeSelect]';
   private readonly dropdownOptionSelector = `mat-option`;
@@ -11,10 +11,13 @@ export class RequestDonorPage {
     return cy.visit(this.baseUrl);
   }
 
-  getUserTitle() {
+  getDonorViewTitle() {
     return cy.get(this.pageTitle);
   }
 
+  getRequestListItems() {
+    return cy.get('.donor-nav-list .request-list-item');
+  }
   selectItemType(value: ItemType) {
     cy.get(this.requestItemTypeDropDown).click();
     return cy.get(`${this.dropdownOptionSelector}[value="${value}"]`);
