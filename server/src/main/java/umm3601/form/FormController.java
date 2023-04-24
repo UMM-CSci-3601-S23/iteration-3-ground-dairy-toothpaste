69957288402b6866082dc6fb8cbb40273ebcf80c
Gregory Peterson <pet03356@morris.umn.edu>
2023-04-24 12:15:23 -0500

    Removed unused sorting on the server side. We sort request forms on the front end.

--- server/src/main/java/umm3601/form/FormController.java
+++ server/src/main/java/umm3601/form/FormController.java
@@ -1,18 +1,11 @@
 package umm3601.form;
 
-import static com.mongodb.client.model.Filters.and;
 import static com.mongodb.client.model.Filters.eq;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-
 import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.model.Sorts;
 import com.mongodb.client.result.DeleteResult;
 
-import org.bson.Document;
 import org.bson.UuidRepresentation;
-import org.bson.conversions.Bson;
 import org.bson.types.ObjectId;
 import org.mongojack.JacksonMongoCollection;
 import java.util.Map;
@@ -45,17 +38,12 @@ public class FormController {
    * @param ctx a Javalin HTTP context
    */
   public void getForms(Context ctx) {
-    Bson combinedFilter = constructFilter(ctx);
-    Bson sortingOrder = constructSortingOrder(ctx);
 
     // All three of the find, sort, and into steps happen "in parallel" inside the
     // database system. So MongoDB is going to find the requests with the specified
     // properties, return those sorted in the specified manner, and put the
     // results into an initially empty ArrayList.
-    ArrayList<Form> matchingRequests = requestCollection
-      .find(combinedFilter)
-      .sort(sortingOrder)
-      .into(new ArrayList<>());
+    ArrayList<Form> matchingRequests = requestCollection.find().into(new ArrayList<>());
 
     // Set the JSON body of the response to be the list of requests returned by the database.
     // According to the Javalin documentation (https://javalin.io/documentation#context),
@@ -67,38 +55,6 @@ public class FormController {
     ctx.status(HttpStatus.OK);
   }
 
-  private Bson constructFilter(Context ctx) {
-    List<Bson> filters = new ArrayList<>(); // start with a blank document
-    /*
-    if (ctx.queryParamMap().containsKey(ITEM_TYPE_KEY)) {
-      String itemType = ctx.queryParamAsClass(ITEM_TYPE_KEY, String.clfoodType and itemType
-      String foodType = ctx.queryParamAsClass(FOOD_TYPE_KEY, String.class)
-        .check(it -> it.matches(FOOD_TYPE_REGEX), "Request must contain valid food type")
-        .get();
-      filters.add(eq(FOOD_TYPE_KEY, foodType));
-    }
-    if (ctx.queryParamMap().containsKey(SORT_ORDER_KEY)){
-      String sortOrder = ctx.queryParamAsClass(SORT_ORDER_KEY, String.class)
-        .check(it -> it.matches(SORT_ORDER_REGEX), "Sort order must be 'oldest' or 'newest")
-        .get();
-      filters.add(eq(SORT_ORDER_KEY, sortOrder));
-    }
-    */
-    // Combine the list of filters into a single filtering document.
-    Bson combinedFilter = filters.isEmpty() ? new Document() : and(filters);
-
-    return combinedFilter;
-  }
-
-  private Bson constructSortingOrder(Context ctx) {
-    // Sort the results. Use the `sortby` query param (default "name")
-    // as the field to sort by, and the query param `sortorder` (default
-    // "asc") to specify the sort order.
-    String sortOrder = Objects.requireNonNullElse(ctx.queryParam("sortOrder"), SORT_ORDER_KEY);
-    Bson sortingOrder = sortOrder.equals("newest")
-    ? Sorts.descending("timeSubmitted") : Sorts.ascending("timeSubmitted");
-    return sortingOrder;
-  }
 
   public void addNewForm(Context ctx) {
     Form newRequest = ctx.bodyAsClass(Form.class);
