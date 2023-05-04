55cf7723d264d2c9eac8da19609b580232ac4b1d
Gregory Peterson <pet03356@morris.umn.edu>
2023-05-04 12:19:28 -0500

    Reworked HTML

--- client/src/app/form/form.service.ts
+++ client/src/app/form/form.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { VolunteerForm, Form } from './form';
+import { VolunteerForm } from './form';
 import { map } from 'rxjs/operators';
 import { FormGroup } from '@angular/forms';
 
--- client/src/app/form/form.ts
+++ client/src/app/form/form.ts
@@ -1,15 +1,5 @@
 ////////////// Interface for the whole form ////////////////////
 
-export interface Form {personalInfo: PersonalInfo;
-  fruitGroup: FruitsType;
-  vegetableGroup: VegetablesType;
-  proteinGroup: ProteinsType;
-  grainGroup: GrainsType;
-  dairyGroup: DairyType;
-  pantryGroup: PantryType;
-  personalGroup: PersonalType;
-  houseHoldGroup: HouseholdType;
-  }
 
   export interface VolunteerForm {personalInfo: PersonalInfo;
     fruit: Fruit;
@@ -18,6 +8,7 @@ export interface Form {personalInfo: PersonalInfo;
     grain: Grain;
     dairy: Dairy;
     pantry: Pantry;
+    baby: Baby;
     personal: Personal;
     houseHold: Household;
     }
@@ -40,7 +31,7 @@ type PersonalInfo = {
 type Fruit = {
   freshMisc: boolean;
         freshAppleJuice: boolean;
-        freshFrozen: boolean;
+        freshFrozenPeaches: boolean;
         cannedMixed: boolean;
         cannedPeaches: boolean;
         cannedAppleSauce: boolean;
@@ -56,24 +47,25 @@ type Vegetable = {
         cannedSweetPotatoes: boolean;
         cannedSpinach: boolean;
         cannedCarrots: boolean;
+        cannedTomatoes: boolean;
+        cannedSpaghettiSauce: boolean;
 };
 type Protein = {
   frozenGroundBeef: boolean;
         frozenGroundBeefPork: boolean;
         frozenPlantBurger: boolean;
-        frozenPizzaRach: boolean;
+        frozenPizzaRanch: boolean;
         frozenVeggieRavioli: boolean;
-        freshChickenDrum: boolean;
-        freshWholeChicken: boolean;
-        freshChickenBreast: boolean;
-        freshChickenLeg: boolean;
-        freshFishSticks: boolean;
-        freshHam: boolean;
-        freshAssortedMeats: boolean;
+        frozenChickenDrum: boolean;
+        frozenWholeChicken: boolean;
+        frozenChickenBreast: boolean;
+        frozenChickenLeg: boolean;
+        frozenFishSticks: boolean;
+        frozenHam: boolean;
+        frozenAssortedMeats: boolean;
         cannedChicken: boolean;
         cannedTuna: boolean;
         cannedSalmon: boolean;
-        cannedMeals: boolean;
         cannedPastaMeatSauce: boolean;
         cannedPastaButterSauce: boolean;
         cannedChili: boolean;
@@ -83,16 +75,26 @@ type Protein = {
         beansYellow: boolean;
         beansPinto: boolean;
         beansPork: boolean;
-        beansRefried: boolean;
-        beansWhite: boolean;
+        driedBlack: boolean;
+        driedPinto: boolean;
+        driedYellow: boolean;
+        driedKidney: boolean;
+        driedMisc: boolean;
+        nutPeanutButter: boolean;
+        nutAlmonds: boolean;
+        nutWalnut: boolean;
+        snackCracker: boolean;
+        snackCookies: boolean;
+        snackMisc: boolean;
 };
 type Grain = {
+  breakfastRice: boolean;
   breakfastStuffing: boolean;
         breakfastPancake: boolean;
         breakfastQuickOat: boolean;
         breakfastCereal: boolean;
         pastaElbow: boolean;
-        pastaMacaronie: boolean;
+        pastaMacaroni: boolean;
         pastaPenne: boolean;
         pastaRiceInstant: boolean;
         bakeryBread: boolean;
@@ -125,6 +127,7 @@ type Pantry = {
         soupMisc: boolean;
         condimentSeasonings: boolean;
         condimentHotSauce: boolean;
+        condimentDressing: boolean;
         condimentRanch: boolean;
         condimentMustard: boolean;
         condimentSyrup: boolean;
@@ -136,7 +139,6 @@ type Baby = {
         babyCereal: boolean;
         babyFormula: boolean;
         babyNewbornGiftBag: boolean;
-        babyDiaper: boolean;
         babyDiaperSize: '';
 };
 
@@ -157,340 +159,4 @@ type Household ={
 };
 
 
-
-///// We have an Object Literal and a type for each form Group. ////
-
-///////////////////////////////////// FRUITS / FRUTAS //////////////////////////////
-
-type FruitsType = {
-  section: string;
-  fresh: {
-    label: string;
-    descriptions: { description: string }[];
-  };
-  canned: {
-    label: string;
-    descriptions: { description: string }[];
-  };
-  dried: {
-    label: string;
-    descriptions: { description: string }[];
-  };
-};
-
-export const fruitLiteral: FruitsType = {
-  section: 'FRUITS / FRUTAS',
-  fresh: {
-    descriptions: [
-      { description: 'Misc Fresh Fruit as available / Varios frutas frescas como disponible' },
-      { description: 'Apple juice / Jugo de manzana' },
-      { description: 'Frozen peaches / Duraznos congelados' }
-    ],
-    label: 'Fresh / Fresca',
-  },
-  canned: {
-    label: 'Canned / En Lata',
-    descriptions: [
-      { description: 'Mixed fruit / Mix de fruta' },
-      { description: 'Peaches / Duraznos' },
-      { description: 'Apple Sauce / Puré de manzana' }
-    ]
-  },
-  dried: {
-    label: 'Dried / Seca',
-    descriptions: [
-      { description: 'Dates (whole OR pieces) / Dátiles (enteros O piezas)' }
-    ]
-  }
-};
-
-/////////////////////////// VEGETABLES / VERDURAS ///////////////////////////////
-type VegetablesType = {
-  section: string;
-  fresh: {
-    label: string;
-    descriptions: { description: string }[];
-  };
-  canned: {
-    label: string;
-    descriptions: { description: string }[];
-  };
-};
-
-export const vegetableLiteral: VegetablesType ={
-  section: 'VEGETABLES / VERDURAS',
-  fresh: {
-    label: 'Fresh / Fresca',
-    descriptions: [
-      { description: 'Carrots / Zanahorias' },
-      { description: 'Potatoes / Papas' },
-      { description: 'Misc veggies as available / Varias verduras como disponible' }
-    ]
-  },
-  canned: {
-    label: 'Canned / En Lata',
-    descriptions: [
-      { description: 'Corn / Elote' },
-      { description: 'Green beans / Ejotes' },
-      { description: 'Peas / Chicharitos' },
-      { description: 'Sweet Potatoes / Papas dulces' },
-      { description: 'Spinach / Espinaca' },
-      { description: 'Carrots / Zanahorias' }
-    ]
-  }
-};
-
-/////////////////////////////////// PROTEINS / PROTEÍNAS //////////////////
-type ProteinsType = {
-  section: string;
-  frozen: {
-    label: string;
-    descriptions: { description: string }[];
-  };
-  fresh: {
-    label: string;
-    descriptions: { description: string }[];
-  };
-  canned: {
-    label: string;
-    descriptions: { description: string }[];
-  };
-  beans: {
-    label: string;
-    descriptions: { description: string }[];
-  };
-};
-
-export const proteinLiteral: ProteinsType = {
-  section: 'PROTEINS / PROTEÍNAS',
-  frozen: {
-    label: 'Frozen meat / Carne congelada',
-    descriptions: [
-      { description: 'Ground beef / Carne de res molido' },
-      { description: 'Ground Beef/Pork Blend / Mezcla de carne de res y puerco molido' },
-      { description: 'Plant based burgers / Hamburguesas vegetarianas' },
-      { description: 'Pizza Ranch Pizza / Pizza de Pizza Ranch' },
-      { description: 'Veggie Ravioli (for one) / Raviolis con verduras (para uno)' }
-    ]
-  },
-  fresh: {
-    label: 'Fresh meat / Carne fresca',
-    descriptions: [
-      { description: 'Chicken drumsticks / Piernas de pollo (pequeños)' },
-      { description: 'Whole chicken / Pollo entero' },
-      { description: 'Chicken breast / Pechuga de pollo' },
-      { description: 'Chicken Leg Qtrs / Piernas de pollo (grande)' },
-      { description: 'Fish Sticks/ Pescado' },
-      { description: 'Ham / Jamón' },
-      { description: 'Assorted meats as available / Varios carnes como disponible' }
-    ]
-  },
-  canned: {
-    label: 'Canned meat / Carne en Lata',
-    descriptions: [
-      { description: 'Chicken / Pollo' },
-      { description: 'Tuna / Atún' },
-      { description: 'Salmon / Salmon' },
-      { description: 'Ready to eat canned meals / Comida instantanea' },
-      { description: 'Pasta with meat sauce / Espagueti en lata con carne' },
-      { description: 'Pasta in butter sauce / Espagueti en lata con mantequilla' },
-      { description: 'Canned chili / Chili en lata' },
-      { description: 'Veg Curry (for one) / Curry con verduras (para uno)' },
-      { description: 'Hot Dog sauce / Salsa para salchichas' }
-    ]
-  },
-  beans: {
-    label: 'Canned beans / Frijoles en Lata',
-    descriptions: [
-      { description: 'Black eyed peas / Frijol carita negro' },
-      { description: 'Yellow eyed beans / Frijoles amarillos' },
-      { description: 'Pinto beans / Frijoles pintos' },
-      { description: 'Pork & beans / Frijoles con puerco' },
-      { description: 'Refried beans / Frijoles refritos' },
-      { description: 'White beans / Frijoles blancos' }
-    ]
-  }
-};
-
-////////////////////// GRAINS / GRANOS ///////////////////
-type GrainsType = {
-  section: string;
-  breakfast: {
-    label: string;
-    descriptions: { description: string }[];
-  };
-  driedPasta: {
-    label: string;
-    descriptions: { description: string }[];
-  };
-  bakery: {
-    label: string;
-    descriptions: { description: string }[];
-  };
-};
-
-export const grainLiteral: GrainsType = {
-  section: 'GRAINS / GRANOS',
-  breakfast: {
-    label: 'Breakfast / Desayuno',
-    descriptions: [
-      { description: 'Stuffing mix / Mezcla para estofada' },
-      { description: 'Pancake mix / Mezcla para hot cakes' },
-      { description: 'Quick Oats / Avena instantánea' },
-      { description: 'Ready-to-eat cereal / Cereal' },
-    ],
-  },
-  driedPasta: {
-    label: 'Dried pasta / Pasta seca',
-    descriptions: [
-      { description: 'Elbow Noodles / Coditos' },
-      { description: 'Macaroni & cheese / Macaroni & queso' },
-      { description: 'Penne pasta / Pasta penne' },
-      { description: 'Instant pasta or rice / Pasta o arroz instantánea' },
-    ],
-  },
-  bakery: {
-    label: 'Bakery / Panaderia',
-    descriptions: [
-      { description: 'Bread / Pan' },
-      { description: 'Hamburger buns / Pan para hamburguesas' },
-      { description: 'Hot dog buns / Pan para Hot Dog' },
-      { description: 'Baked goods / Postres' },
-    ],
-  },
-};
-
-//////////////////////////// MILK & CHEESE / LÁCTEOS ///////////////////
-
-type DairyType = {
-  section: string;
-  fresh: {
-    label: string;
-    descriptions: {
-      description: string;
-    }[];
-  };
-};
-
-export const dairyLiteral: DairyType = {
-  section: 'MILK & CHEESE / LÁCTEOS',
-  fresh: {
-    label: '',
-    descriptions: [
-      { description: 'Fresh milk (lactose free) / Leche (sin lactosa)' },
-      { description: 'Misc dairy products / Varios productos lácteos' },
-      { description: 'Cheese / Queso' },
-      { description: 'Yogurt / Yogur' },
-      { description: 'Butter / Mantequilla' },
-      { description: 'Shelf stable milk / Leche que no necesita refrigeración' }
-    ]
-  },
-};
-
-///// PANTRY, COOKING, & BAKING / COCINAR y HORNEAR ///////
-
-
-type PantryType = {
-  section: string;
-  baking: {
-    label: string;
-    descriptions: { description: string }[];
-  };
-  soups: {
-    label: string;
-    descriptions: { description: string }[];
-  };
-  condiments: {
-    label: string;
-    descriptions: { description: string }[];
-  };
-};
-
-export const pantryLiteral: PantryType = {
-  section: 'PANTRY, COOKING, & BAKING / COCINAR y HORNEAR',
-  baking: {
-    label: 'Baking / Hornear',
-    descriptions: [
-      { description: 'Baking mix / Mezcla para panecillos' },
-      { description: 'Cake mix / Mezcla para pastel' },
-      { description: 'Flour / Harina' },
-      { description: 'Muffin mix / O mezcla para panquecitos' },
-      { description: 'Cookie mix / Mezcla para galletas' },
-      { description: 'Miscellaneous baking items / Varios productos para hornear' },
-      { description: 'Vegetable oil / Aceite vegetal' }
-    ]
-  },
-  soups: {
-    label: 'Soups / Sopas',
-    descriptions: [
-      { description: 'Chicken noodle / Espagueti con pollo' },
-      { description: 'Tomato soup / Crema de jitomate' },
-      { description: 'Vegetable soup / Sopa de vegetales' },
-      { description: 'Creamy canned soup / Crema de hongos o pollo' },
-      { description: 'Misc soups, as available / Varios sopas en lata, disponibilidad variable' }
-    ]
-  },
-  condiments: {
-    label: 'Condiments / Condimentos',
-    descriptions: [
-      { description: 'Seasonings / Condimentos' },
-      { description: 'Hot Sauce / Salsa picante' },
-      { description: 'Salad dressing (miscellaneous) / Varios aderezos' },
-      { description: 'Ranch dressing / Aderezo “Ranch”' },
-      { description: 'Mustard / Mostaza' },
-      { description: 'Syrup / Jarabe para panqueques' },
-      { description: 'Misc pickles, olives, etc / Varios encurtidos, olivas, etc.' }
-    ]
-  }
-};
-
-
-
-/////////////////////////////// PERSONAL / PERSONAL ////////////////////////
-
-type PersonalType = {
-  section: string;
-  hygiene: {
-    label: string;
-    descriptions: { description: string }[];
-  };
-};
-
-export const personalLiteral: PersonalType = {
-  section: 'PERSONAL / PERSONAL',
-  hygiene: {
-    label: 'Hygiene / Higiene',
-    descriptions: [
-      { description: 'Shampoo / Shampu' },
-      { description: 'Body or hand soap / Jabón para el cuerpo o manos' },
-      { description: 'Toothpaste / Pasta dental' },
-      { description: 'Toothbrushes / Cepillo dental' },
-      { description: 'Birthday Party Kit / Kit para una fiesta de cumpleaños' },
-      { description: 'Hand sanitizer / Desinfectante de manos' },
-      { description: 'Feminine hygiene / Artículos higiénicos para mujer' }
-    ]
-  }
-};
-
-////////////////////////////////// HOUSEHOLD / PARA LA CASA //////////////////////////
-
-type HouseholdType = {
-  section: string;
-  cleaning: {
-    label: string;
-    descriptions: { description: string }[];
-  };
-};
-
-export const householdLiteral: HouseholdType = {
-  section: 'HOUSEHOLD / PARA LA CASA',
-  cleaning: {
-    label: '',
-    descriptions: [
-      { description: 'Dish soap / Jabón para trastes' },
-      { description: 'Laundry detergent / Detergente para ropa' },
-      { description: 'Disinfecting wipes / Toallitas desinfectantes' }
-    ]
-  }
-};
 
